fc7ff33c1fb156029f34cefbc30f56cf12398604
viswavisan <viswaa1990@yahoo.com>
2026-01-24 15:56:10 +0530

    update

--- Interview_evaluation/main.py
+++ Interview_evaluation/main.py
@@ -136,4 +136,4 @@ def save_answers(applicant_id, applicant_name, questions):
     return 'saved successfully'
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
