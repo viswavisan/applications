6329520a19358a4b672204be56a210f771149237
viswavisan <viswaa1990@yahoo.com>
2026-01-30 14:06:38 +0530

    update

--- Interview_evaluation/test_cases/unit_test/test_case1.py
+++ Interview_evaluation/test_cases/unit_test/test_case1.py
@@ -1,6 +1,6 @@
 # test_sample.py
 
-def test_addition():
-    result = 2 + 2
+def test_addition2():
+    result = 2 + 3
     # This assertion will fail
-    assert result == 4
+    assert result == 5
