3f481fb59f0b137b2be05518e159ebc7dbaf542d
viswavisan <viswaa1990@yahoo.com>
2026-04-11 16:40:08 +0530

    add fit mafia application

--- fit_mafia/app.py
+++ fit_mafia/app.py
@@ -7,6 +7,6 @@ from flask import Flask, render_template, Blueprint, request
 template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates'))
 app = Blueprint('fit_mafia', __name__, template_folder=template_dir)
 
-@app.route('/test', methods=['GET'])
+@app.route('/fitmafia', methods=['GET'])
 def test():
     return render_template('test.html')
