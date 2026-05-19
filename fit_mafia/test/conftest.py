abb15fca31f751cd6503a1a81f5cd5f607d601c0
viswavisan <viswaa1990@yahoo.com>
2026-05-19 23:25:07 +0530

    optimze code structure

--- fit_mafia/test/conftest.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-
-# Add the project root directory to the Python path to allow pytest to find the 'fit_mafia' module
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-sys.path.insert(0, project_root)
