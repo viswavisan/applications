5bac07af6c2394ba5ecf59cd7a7c21bb9a04589d
viswavisan <viswaa1990@yahoo.com>
2026-05-20 09:00:26 +0530

    optimze code structure

--- fit_mafia/test/app.py
+++ fit_mafia/test/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 def create_app():
     test_app = Flask(__name__)
     test_app.config['TESTING'] = True
+    app.config['SECRET_KEY'] = 'test_secret_key'
     test_app.config['WTF_CSRF_ENABLED'] = False
     return test_app
 
